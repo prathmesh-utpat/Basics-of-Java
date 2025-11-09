d057b7ec9b3c6be99e6fe3413cdf459569c9fdba
prathmesh <prathmeshutpat88@gmail.com>
2025-11-09 19:28:26 +0530

    Variables.java

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 10;
+        System.out.println(a);
+        int sum = a+b;
+        System.out.println(sum);
+    }
+
+}
