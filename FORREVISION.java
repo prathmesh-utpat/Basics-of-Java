e3daeef02514c5eb1038b65752628ba4f885f5cd
prathmesh <prathmeshutpat88@gmail.com>
2026-01-08 07:55:09 +0530

    Added FORREVISION program

---
+++ FORREVISION.java
@@ -0,0 +1,17 @@
+public class FORREVISION {
+    public static void main(String args[]) {
+        int a=10;
+        char b='A';
+        float c=5.5f;
+        boolean d=true;
+        String e="Hello";
+        double f=20.99;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
