79c15f8f40a4c8f1db634bed21dd3b5e96e3cb97
prathmesh <prathmeshutpat88@gmail.com>
2026-01-08 08:09:07 +0530

    inputs in java

--- FORREVISION.java
+++ FORREVISION.java
@@ -1,17 +1,15 @@
+import java.util.*;
 public class FORREVISION {
-    public static void main(String args[]) {
-        int a=10;
-        char b='A';
-        float c=5.5f;
-        boolean d=true;
-        String e="Hello";
-        double f=20.99;
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
+        System.out.println("enter a number ");
+        int a = sc.nextInt();
+        
+        System.out.println("enter a number ");
+        int b = sc.nextInt();
+
+        int sum = a+b;
+        System.out.println("the sum is " + sum);
     }
 }
