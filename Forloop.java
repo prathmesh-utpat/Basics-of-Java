9876ddbd2ef829d301f804f5549bf7a900828f4c
prathmesh <prathmeshutpat88@gmail.com>
2026-01-09 08:33:17 +0530

    Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,16 @@
+public class Forloop {
+    public static void main(String args[]) {
+
+        // for loop syntax
+        for (int i = 1; i <= 10; i++) {
+         
+            System.out.println("hello ");
+        }
+
+        for (int line=1; line <=4; line++) {
+            System.out.println("****");
+        }
+
+       
+    }
+}
